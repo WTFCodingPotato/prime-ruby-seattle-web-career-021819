07c3bf37473b759553ae117297d06cd399f1a50d
Joshua M Loran <joshua.loran@gmail.com>
2019-01-09 20:03:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(number)
+  if (number == 1)
+    return true
+  end
+  if (number < 1)
+    return false
+  end
+  for item in 2...number
+    if(number % item == 0)
+      return false
+    end
+  end
+  return true
+end
